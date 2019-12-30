03849df4b19301cb9a21176d660dfc135597aecd
Frank Su <candyline25@gmail.com>
2019-12-30 00:06:50 -0800

    Add map view

--- Foodventures/ViewController.swift
+++ Foodventures/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
+        
+        let mapView : MKMapView = {
+            let map = MKMapView()
+            map.translatesAutoresizingMaskIntoConstraints = false
+            return map
+        }()
+        
+        // add subviews before constraints or throw anchors reference items in different view hierarchies? That's illegal
+        view.addSubview(mapView)
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        
     }
 
 
