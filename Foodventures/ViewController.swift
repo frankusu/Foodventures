8617cdcf421ad59232dff372d2c114367b267051
Frank Su <candyline25@gmail.com>
2019-12-29 19:54:40 -0800

    Initial Commit

---
+++ Foodventures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foodventures
+//
+//  Created by Frank Su on 2019-12-29.
+//  Copyright © 2019 frankusu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
