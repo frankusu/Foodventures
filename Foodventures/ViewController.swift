d700d28eb5bccc2e3bd9eb061f06e09ef4cac9aa
Frank Su <candyline25@gmail.com>
2019-12-30 17:36:34 -0800

    Map sets and shows user current location with permission

--- Foodventures/ViewController.swift
+++ Foodventures/ViewController.swift
@@ -11,14 +11,18 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    let locationManager = CLLocationManager()
+    
     let mapView : MKMapView = {
         let map = MKMapView()
         
         map.translatesAutoresizingMaskIntoConstraints = false
-        let initialLocation = CLLocation(latitude: 49.246292, longitude: -123.116226)
-        let regionRadius: CLLocationDistance = 1000
-        let coordinateRegion = MKCoordinateRegion(center: initialLocation.coordinate, latitudinalMeters: regionRadius,longitudinalMeters: regionRadius)
-        map.setRegion(coordinateRegion, animated: true)
+        map.showsUserLocation = true
+//        let initialLocation = CLLocation(latitude: 49.246292, longitude: -123.116226)
+//        let regionRadius: CLLocationDistance = 1000
+//        let coordinateRegion = MKCoordinateRegion(center: initialLocation.coordinate, latitudinalMeters: regionRadius,longitudinalMeters: regionRadius)
+//        map.setRegion(coordinateRegion, animated: true)
         
         return map
     }()
@@ -33,22 +37,56 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
+        setUpViews()
+        setUpCurrentLocation()
+        
+    }
+
+    //MARK: Methods
+    
+    func setUpCurrentLocation() {
+        // could take time for requested info to come back, use delegate to handle asynch
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+//        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+//            mapView.showsUserLocation = true
+//        } else {
+//            locationManager.requestWhenInUseAuthorization()
+//        }
+    }
+
+    private func setUpViews() {
         // add subviews before constraints or throw anchors reference items in different view hierarchies? That's illegal
         view.addSubview(mapView)
-        
-        
-        
+    
         NSLayoutConstraint.activate([
             mapView.topAnchor.constraint(equalTo: view.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
-        
-        
-        
     }
-
-
 }
 
+extension ViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let regionRadius: CLLocationDistance = 1000
+            let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius,longitudinalMeters: regionRadius)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error)")
+    }
+    
+}
